cfc862fb2403e3de161a5ce2805493e3efbc2621
羅翡幸 <feliciaalo08@gmail.com>
2023-05-10 14:54:23 +0800

    validate:by_using_customize_title_need_included_the_word_cool

--- controller/video_controller.go
+++ controller/video_controller.go
@@ -3,7 +3,9 @@ package controller
 import (
 	"github.com/feliciacia/go-gin-framework/go-gin-framework/entity"
 	"github.com/feliciacia/go-gin-framework/go-gin-framework/service"
+	"github.com/feliciacia/go-gin-framework/go-gin-framework/validators"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type VideoController interface {
@@ -15,7 +17,11 @@ type controller struct {
 	service service.VideoService
 }
 
+var validate *validator.Validate
+
 func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
 	return &controller{
 		service: service,
 	}
@@ -28,6 +34,10 @@ func (c *controller) FindAll() []entity.Video {
 func (c *controller) Save(ctx *gin.Context) error {
 	var video entity.Video
 	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
 	if err != nil {
 		return err
 	}
